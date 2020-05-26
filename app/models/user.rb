10c113922ca2fda0fb5f42970368b1bf1e8e7017
James Oddy <jamesoddy8@gmail.com>
2020-05-26 18:13:02 +0100

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+
+  validates_uniqueness_of :username
+
+  def self.generate
+    adjectives = ['Ancient', 'Benevolent', 'Bubbling', 'Cunning', 'Dangerous', 'Exotic', 'Fluttering', 'Flying', 'Gilded', 'Misty', 'Mystical', 'Sparkling', 'Wise']
+    nouns = ['Waterfall', 'River', 'Tiger', 'Panda', 'Tree', 'Owl', 'Fox', 'Glacier', 'Sprite', 'Butterfly', 'Mountain', 'Midnight']
+    number = rand.to_s[2..4]
+    username ="#{adjectives.sample}-#{nouns.sample}-#{number}"
+    create(username: username)
+  end
+
+end
