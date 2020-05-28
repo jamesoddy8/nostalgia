be03e6c804f57f5328c50a4cbe5b161dd1c3ad50
James Oddy <jamesoddy8@gmail.com>
2020-05-28 22:12:25 +0100

    action cable working

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   def self.generate
     adjectives = ['Ancient', 'Benevolent', 'Bubbling', 'Cunning', 'Dangerous', 'Exotic', 'Fluttering', 'Flying', 'Gilded', 'Misty', 'Mystical', 'Sparkling', 'Wise']
-    nouns = ['Waterfall', 'River', 'Tiger', 'Panda', 'Tree', 'Owl', 'Fox', 'Glacier', 'Sprite', 'Butterfly', 'Mountain', 'Midnight']
+    nouns = ['Waterfall', 'River', 'Tiger', 'Panda', 'Tree', 'Owl', 'Fox', 'Glacier', 'Sprite', 'Butterfly', 'Mountain', 'Midnight', 'Cactus']
     number = rand.to_s[2..4]
     username ="#{adjectives.sample}-#{nouns.sample}-#{number}"
     create(username: username)
